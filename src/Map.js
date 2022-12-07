e826b0f48b4daa96e61019cdcf94870667cee501
Tanguy Hauguel <tanguy.hauguel@etudiant.univ-lr.fr>
2022-12-07 14:01:30 +0100

    first map

--- src/Map.js
+++
@@ -1,6 +0,0 @@
-import Map from 'ol/Map';
-import View from 'ol/View';
-
-export class MyMap extends React.Component {
-
-}
--- src/index.js
+++ src/index.js
@@ -1,48 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import Map from "ol/Map";
-import View from "ol/View";
+import React from "react";
+import { render } from "react-dom";
+import MyMap from "./MyMap";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-class MyMap extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            map: new Map({
-                target: null,
-                layers: [
-                    // Ajoutez vos couches ici
-                ],
-                view: new View({
-                    center: [0, 0],
-                    zoom: 0
-                })
-            })
-        }
-    }
-
-    componentDidMount() {
-        this.state.map.setTarget('map');
-    }
-
-    render() {
-        return <div id="map" style={{ width: "100%", height: "100%" }}></div>;
-    }
-}
-
-root.render(
-
-  <React.StrictMode>
-    <MyMap />
-  </React.StrictMode>
+const App = () => (
+    <div>
+        <MyMap />
+    </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+render(<App />, document.getElementById("root"));
